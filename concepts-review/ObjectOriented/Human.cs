876948171a796df1dd66ee59ee935c61ef4222fa
wilsonCarv <alfaro9331@gmai.com>
2024-11-22 14:21:49 -0600

    object oriented

---
+++ concepts-review/ObjectOriented/Human.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace concepts_review.ObjectOriented
+{
+    public class Human
+    {
+        public string? FirstName { get; set; }
+        public int Age { get; set; }
+        private double Salary { get; set; }
+        private string? _JobTitle { get; set; }
+        public string JobTitle
+        {
+            get
+            {
+                return _JobTitle ?? "Unknown";
+            }
+            set
+            {
+                if(Salary > 0)
+                {
+                    _JobTitle = value;
+                }
+            }
+        }
+
+        public Human(string? firstName, int age)
+        {
+            FirstName = firstName;
+            Age = age;
+        }
+
+        public Human()
+        {
+        }
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, my name is {FirstName} and I am {Age} years old");
+        }
+        ~Human()
+        {
+            Console.WriteLine("Human destroyed");
+        }
+
+    }
+}
