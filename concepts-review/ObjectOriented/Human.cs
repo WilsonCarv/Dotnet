127cf818874ea1ff907a57a5a7c8f81ec628645f
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-11-25 14:39:15 -0600

    clean up class

--- concepts-review/ObjectOriented/Human.cs
+++ concepts-review/ObjectOriented/Human.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace concepts_review.ObjectOriented
+﻿namespace concepts_review.ObjectOriented
 {
     public class Human
     {
