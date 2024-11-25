be58016b86095486a32f7596815e17bcdf7af11c
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-11-25 14:36:08 -0600

    clean up class

--- concepts-review/ArraysReview/ArraysReview.cs
+++ concepts-review/ArraysReview/ArraysReview.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace concepts_review.ArraysReview
 {
@@ -11,9 +6,7 @@ namespace concepts_review.ArraysReview
     {
         public static void PrintArrayList()
         {
-            ArrayList arrayList = new ArrayList();
-            arrayList.Add("Hello");
-            arrayList.Add("World");
+            ArrayList arrayList = ["Hello", "World"];
             Console.WriteLine("ArrayList: {0}", string.Join(", ", arrayList));
             foreach (var item in arrayList)
             {
